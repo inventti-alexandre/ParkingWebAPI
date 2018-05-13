459cb2cf525d8197f281b81a0091e2532eb9902e
tetianamarkova <tetyana.markova@gmail.com>
2018-05-13 12:17:10 +0300

    Add ClassLibrary (fixes #2 )

---
+++ ParkingLogic/ParkingLogic/Settings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLogic
+{
+    public static class Settings
+    {
+        private static Dictionary<CarType, double> PriceList = new Dictionary<CarType, double>()
+        {
+            {CarType.Motorcycle, 1},
+            {CarType.Bus, 2},
+            {CarType.Passenger, 3},
+            {CarType.Truck, 4}
+        };
+
+        public static Dictionary<CarType, double> getPriceList()
+        {
+            return PriceList;
+        }
+
+        public static void changePriceList(Dictionary<CarType, double> priceList)
+        {
+            PriceList = priceList;
+        }
+
+        public static int Timeout { get; set; } = 3000;
+
+        public static int ParkingSpace { get; set; } = 20;
+
+        public static double Fine { get; set; } = 2;
+
+
+    }
+}
